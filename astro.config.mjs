1b968c0ec70c58022c654710dee9775b40264268
FireIsGood <fireisadequate@gmail.com>
2024-04-23 14:59:13 -0700

    Add GH Pages config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,8 @@
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  outDir: "./docs",
+  site: "https://fireisgood.github.io/",
+  base: "/astro-testing",
+});
